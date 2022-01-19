7544a2f8656cfad6e462e56d4dd5cb7eda55f702
rajahmad-akratech <rajahmad@akratech.com>
2022-01-19 22:52:10 +0530

    Foreqast - added flask routing for article, log-in, sign-up, terms and conditions and privacy policy

--- modules/landing/web/landing.py
+++ modules/landing/web/landing.py
@@ -25,3 +25,30 @@ def foreqast_sign_up():
 	except Exception as e:
 		print(str(e))
 		return 'OOPS !!!, failed to load the sign-up page ...'
+
+# handle artical details page routing
+@app.route('/foreqast_article_detail', methods = ['GET'])
+def foreqast_article_detail():
+	try:
+		return render_template('/landing/article-details.html')
+	except Exception as e:
+		print(str(e))
+		return 'OOPS !!!, failed to load the artical detail page ...'
+
+# handle terms and conditions page routing
+@app.route('/foreqast_terms_and_conditions', methods = ['GET'])
+def foreqast_terms_and_conditions():
+	try:
+		return render_template('/landing/terms-conditions.html')
+	except Exception as e:
+		print(str(e))
+		return 'OOPS !!!, failed to load the terms and conditions page ...'
+
+# handle privacy policy page routing
+@app.route('/foreqast_privacy_policy', methods = ['GET'])
+def foreqast_privacy_policy():
+	try:
+		return render_template('/landing/privacy-policy.html')
+	except Exception as e:
+		print(str(e))
+		return 'OOPS !!!, failed to load the privacy policy page ...'
