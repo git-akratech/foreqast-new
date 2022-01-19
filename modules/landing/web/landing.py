761d58b9c1575867ef799eea5c4fc65e15cb186d
rajahmad-akratech <rajahmad@akratech.com>
2022-01-19 20:41:09 +0530

    ForeQast- Added login page redirection

--- modules/landing/web/landing.py
+++ modules/landing/web/landing.py
@@ -1,7 +1,18 @@
 
+# Handle landing page routing
 @app.route('/', methods = ['GET'])
 def landing():
 	try:
 		return render_template('/landing/index.html')
 	except Exception as e:
+		print(str(e))
 		return 'OOPS !!!, failed to load the landing page ...'
+
+# Handle login page routing
+@app.route('/foreqast_login', methods = ['GET'])
+def foreqast_login():
+	try:
+		return render_template('/landing/log-in.html')
+	except Exception as e:
+		print(str(e))
+		return 'OOPS !!!, failed to load the login page ...'
