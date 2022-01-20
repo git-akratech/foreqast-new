8ec9ebfde925e300e0318822910a9d772b33f060
rajahmad-akratech <rajahmad@akratech.com>
2022-01-20 23:17:38 +0530

    Foreqast- updated landing pages

--- modules/load_data/common/load_data.py
+++ modules/load_data/common/load_data.py
@@ -13,7 +13,7 @@ def get_load_data_by_avg_with_date_range(data):
 
 		# select data query
 		data_list = []
-		select_query = "select date_format(`timestamp`, '%%Y-%%m-%%d') as running_date, round(avg(load_MW), 2) as load_data, ba_name from foreqast_load_data group by date_format(`timestamp`, '%%Y-%%m-%%d') having ba_name = 'PJM' and running_date between '2021-12-27' and '2022-01-10'"
+		select_query = "select date_format(`timestamp`, '%%Y-%%m-%%d') as running_date, round(avg(load_MW), 2) as load_data, ba_name from foreqast_load_data group by date_format(`timestamp`, '%%Y-%%m-%%d') having ba_name = 'PJM' and running_date between '2021-12-15' and '2022-01-10'"
 		result = app._engine.execute(select_query)
 		if result.rowcount > 0:
 			for row in result:
